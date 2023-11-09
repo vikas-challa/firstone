30cd6f35f159388199c456be95895b13ef0cc701
VikasReddy 401 <vikas-challa>
2023-11-09 10:58:44 +0530

    done with first one

---
+++ src/main/java/cloudtech/App.java
@@ -0,0 +1,13 @@
+package cloudtech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
